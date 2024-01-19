36b144fc4ba512d919980c087ae588a9bc8dd573
Riku <134082872+rigozu9@users.noreply.github.com>
2024-01-19 17:01:53 +0200

    7.6 done

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -2,9 +2,9 @@
 import { useField } from '../hooks'
 
 const CreateNew = (props) => {
-  const { ...content } = useField('text')
-  const { ...author } = useField('text')
-  const { ...info } = useField('text')
+  const { reset: resetContent, ...content } = useField('text')
+  const { reset: resetAuthor, ...author } = useField('text')
+  const { reset: resetInfo, ...info } = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -18,9 +18,9 @@ const CreateNew = (props) => {
 
   const handleReset = (e) => {
     e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
